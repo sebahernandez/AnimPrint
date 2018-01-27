203794797e2c3657879721ac420d5d1c17d44524
sebastian <sebaprogramer@gmail.com>
2018-01-27 01:32:18 -0300

    elimino archivos innecesarios

--- archivos/controles-efectos-frontend-master/hamburger/main.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  transformicons.add(".tcon");
-
-  document.querySelector(".toggle-menu").addEventListener("click",function(){
-    document.querySelector(".main-container").classList.toggle("open");
-  })
-})()
